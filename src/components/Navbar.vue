53a39b3c53373adcd058cd3e87616e8db78f8b42
Laurin49 <welter@diwe.info>
2024-02-18 19:38:06 +0100

    abs10 Router o.k.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
       <div class="container py-2">
-          <a href="#" class="navbar-brand">
-              <span>Todo</span>
-              <strong>List</strong>
-          </a>
+            <router-link :to="{ name: 'home' }" class="navbar-brand">
+                <span>Todo</span>
+                <strong>List</strong>
+            </router-link>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
               aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
@@ -12,18 +12,19 @@
           <div class="collapse navbar-collapse" id="navbarNav">
               <ul class="navbar-nav">
                   <li class="nav-item">
-                      <a href="my-tasks.html" class="nav-link">Tasks</a>
+                    <router-link :to="{ name: 'tasks' }" class="nav-link">Tasks</router-link>
+                    <!-- <a href="#" @click.prevent="$router.push('/tasks')" class="nav-link">Tasks</a> -->
                   </li>
                   <li class="nav-item">
-                      <a href="#" class="nav-link">Summary</a>
+                    <router-link :to="{ name: 'summary' }" class="nav-link">Summary</router-link>
                   </li>
               </ul>
               <ul class="navbar-nav ms-auto">
                   <li class="nav-item">
-                      <a href="#" class="btn btn-outline-secondary ms-2">Login</a>
+                    <router-link :to="{ name: 'login' }" class="btn btn-outline-secondary ms-2">Login</router-link>
                   </li>
                   <li class="nav-item">
-                      <a href="#" class="btn btn-danger ms-2">Register</a>
+                    <router-link :to="{ name: 'register' }" class="btn btn-danger ms-2">Register</router-link>
                   </li>
                   <li class="nav-item">
                       <a href="#" class="btn btn-outline-secondary ms-2">Logout</a>
@@ -33,3 +34,9 @@
       </div>
   </nav>
 </template>
+<style scoped>
+.nav-link.router-link-active {
+    color: rgba(0,0,0,.9);
+    font-weight: bold;
+}
+</style>
